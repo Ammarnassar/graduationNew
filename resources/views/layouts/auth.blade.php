21d9e6d43b2b5f8d0d3bcfbfadf41968a2c55ddb
Ammarnassar <ammarnassar530@gmail.com>
2021-03-09 19:43:44 +0200

    first

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>SocialV - Responsive Bootstrap 4 Admin Dashboard Template</title>
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="temp/html/images/favicon.ico" />
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="temp/html/css/bootstrap.min.css">
+    <!-- Typography CSS -->
+    <link rel="stylesheet" href="temp/html/css/typography.css">
+    <!-- Style CSS -->
+    <link rel="stylesheet" href="temp/html/css/style.css">
+    <!-- Responsive CSS -->
+    <link rel="stylesheet" href="temp/html/css/responsive.css">
+</head>
+<body>
+<!-- loader Start -->
+<div id="loading">
+    <div id="loading-center">
+    </div>
+</div>
+<!-- loader END -->
+<!-- Sign in Start -->
+<section class="sign-in-page">
+    <div id="container-inside">
+        <div id="circle-small"></div>
+        <div id="circle-medium"></div>
+        <div id="circle-large"></div>
+        <div id="circle-xlarge"></div>
+        <div id="circle-xxlarge"></div>
+    </div>
+    <div class="container p-0">
+        <div class="row no-gutters">
+            <div class="col-md-6 text-center pt-5">
+                <div class="sign-in-detail text-white">
+                    <a class="sign-in-logo mb-5" href="#"><img src="temp/html/images/logo-full.png" class="img-fluid" alt="logo"></a>
+                    <div class="owl-carousel" data-autoplay="true" data-loop="true" data-nav="false" data-dots="true" data-items="1" data-items-laptop="1" data-items-tab="1" data-items-mobile="1" data-items-mobile-sm="1" data-margin="0">
+                        <div class="item">
+                            <img src="temp/html/images/login/1.png" class="img-fluid mb-4" alt="logo">
+                            <h4 class="mb-1 text-white">Manage your orders</h4>
+                            <p>It is a long established fact that a reader will be distracted by the readable content.</p>
+                        </div>
+                        <div class="item">
+                            <img src="temp/html/images/login/1.png" class="img-fluid mb-4" alt="logo">
+                            <h4 class="mb-1 text-white">Manage your orders</h4>
+                            <p>It is a long established fact that a reader will be distracted by the readable content.</p>
+                        </div>
+                        <div class="item">
+                            <img src="temp/html/images/login/1.png" class="img-fluid mb-4" alt="logo">
+                            <h4 class="mb-1 text-white">Manage your orders</h4>
+                            <p>It is a long established fact that a reader will be distracted by the readable content.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            @yield('content')
+        </div>
+    </div>
+</section>
+<!-- Sign in END -->
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="temp/html/js/jquery.min.js"></script>
+<script src="temp/html/js/popper.min.js"></script>
+<script src="temp/html/js/bootstrap.min.js"></script>
+<!-- Appear JavaScript -->
+<script src="temp/html/js/jquery.appear.js"></script>
+<!-- Countdown JavaScript -->
+<script src="temp/html/js/countdown.min.js"></script>
+<!-- Counterup JavaScript -->
+<script src="temp/html/js/waypoints.min.js"></script>
+<script src="temp/html/js/jquery.counterup.min.js"></script>
+<!-- Wow JavaScript -->
+<script src="temp/html/js/wow.min.js"></script>
+<!-- lottie JavaScript -->
+<script src="temp/html/js/lottie.js"></script>
+<!-- Apexcharts JavaScript -->
+<script src="temp/html/js/apexcharts.js"></script>
+<!-- Slick JavaScript -->
+<script src="temp/html/js/slick.min.js"></script>
+<!-- Select2 JavaScript -->
+<script src="temp/html/js/select2.min.js"></script>
+<!-- Owl Carousel JavaScript -->
+<script src="temp/html/js/owl.carousel.min.js"></script>
+<!-- Magnific Popup JavaScript -->
+<script src="temp/html/js/jquery.magnific-popup.min.js"></script>
+<!-- Smooth Scrollbar JavaScript -->
+<script src="temp/html/js/smooth-scrollbar.js"></script>
+<!-- Chart Custom JavaScript -->
+<script src="temp/html/js/chart-custom.js"></script>
+<!-- Custom JavaScript -->
+<script src="temp/html/js/custom.js"></script>
+</body>
+</html>
